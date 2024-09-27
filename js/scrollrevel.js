d35cd8731d1afe73ca2070018757a6bc1984c7a7
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-09-27 08:18:32 -0300

    ScrollRevel

---
+++ js/scrollrevel.js
@@ -0,0 +1,69 @@
+ScrollReveal({ reset: true });
+
+/*Sessao de textos*/
+    ScrollReveal().reveal('#SessaoTexto', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'bottom',
+        }
+    );
+
+    ScrollReveal().reveal('#Img', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'top',
+        }
+    );
+/*Sessao de textos*/
+/*Cards*/
+    ScrollReveal().reveal('#Cards', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'top',
+        }
+    );
+    ScrollReveal().reveal('#Cards02', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'bottom',
+        }
+    );
+    ScrollReveal().reveal('#Cards03', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'top',
+        }
+    );
+    ScrollReveal().reveal('#Cards04', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'bottom',
+        }
+    );
+    ScrollReveal().reveal('#Cards05', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'top',
+        }
+    );
+/*Cards*/
+/*Img Clientes/Parceiros*/
+ScrollReveal().reveal('#ClienteParceiros', 
+    {    
+        duration: 1000, reset: true, distance: '100px' , origin: 'top',
+    }
+);
+ScrollReveal().reveal('#ClienteParceiros', 
+    {    
+        duration: 1000, reset: true, distance: '100px' , origin: 'bottom',
+    }
+);
+ScrollReveal().reveal('#ClienteParceiros', 
+    {    
+        duration: 1000, reset: true, distance: '100px' , origin: 'top',
+    }
+);
+ScrollReveal().reveal('#ClienteParceiros', 
+    {    
+        duration: 1000, reset: true, distance: '100px' , origin: 'bottom',
+    }
+);
+ScrollReveal().reveal('#ClienteParceiros', 
+    {    
+        duration: 1000, reset: true, distance: '100px' , origin: 'top',
+    }
+);
+/*Img Clientes/Parceiros*/
