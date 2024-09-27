ebaa4d1cf407ba38fcb512a11962c891f51c1fa0
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-09-27 12:47:25 -0300

    Crds css

--- index.php
+++ index.php
@@ -127,7 +127,7 @@
                         <!--Cards-->
                             <div id="SessaoCards" class="d-flex justify-content-between container-md p-4">
                                 <!--Markplaces-->
-                                    <div id="Cards" class="card bg-black d-flex justify-content-center align-items-center text-center w-25" style="">
+                                    <div id="Cards" class="card bg-black d-flex justify-content-center align-items-center text-center w-25">
                                             <svg class="p-3" id="Grupo_53" data-name="Grupo 53" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 226.687 202.402">
                                                 <path id="Caminho_79" data-name="Caminho 79" d="M-2465.353-484.781h-125.459a12.57,12.57,0,0,0-12.316,15.083l16.453,80.645A12.57,12.57,0,0,0-2574.358-379h92.553a12.57,12.57,0,0,0,12.316-10.056l16.453-80.645A12.569,12.569,0,0,0-2465.353-484.781Z" transform="translate(2679.465 518.522)" fill="#fff" style="
                                                 background-image: linear-gradient(135deg,var(--ast-global-color-2) 0%,var(--ast-global-color-1) 100%);
@@ -146,7 +146,7 @@
                                                 <path id="Caminho_85" data-name="Caminho 85" d="M-2510.2-462.291q-15.181,27.745-30.363,55.5c-1.287,2.377-3.581,4.527-6.418,4.6-2.7.14-5.02-1.829-6.229-4.094q-17.049-31-34.112-61.98c-1.506-2.491-1.706-6.055.485-8.227a7.523,7.523,0,0,1,4.542-2.12q11.715,21.3,23.439,42.593a5.482,5.482,0,0,0,4.811,3.169c2.188-.057,3.956-1.715,4.948-3.552q10.624-19.455,21.237-38.92l11.072.714a8.571,8.571,0,0,1,6.859,3.695C-2508.238-468.315-2508.708-464.872-2510.2-462.291Zm-27.508-12.447a3.736,3.736,0,0,0-2.986-1.611q-13.742-.892-27.489-1.784a3.718,3.718,0,0,0-3,.952,2.96,2.96,0,0,0-.209,3.58q7.422,13.5,14.859,27a3.085,3.085,0,0,0,2.712,1.787,3.493,3.493,0,0,0,2.792-2.005q6.606-12.076,13.2-24.161A3.664,3.664,0,0,0-2537.7-474.738Z" transform="translate(2700.042 527.03)" fill="#cb6ce6"></path>
                                             </svg>
                                         <div class="card-body">
-                                            <span style="color: var(--roxo-vendex); font-weight: 700;" class="CardTexto">
+                                            <span class="CardTexto">
                                                 <h5 class="card-title">Gestão de</h5>
                                                 <h5 class="card-title">Marketplace</h5>
                                             </span>
@@ -157,10 +157,10 @@
                                     </div>
                                   <!--Markplaces-->
                                   <!--Consultoria-->
-                                    <div id="Cards02" class="card bg-black text-center d-flex justify-content-center" style="width: 18rem;">
+                                    <div id="Cards02" class="card bg-black text-center d-flex justify-content-center  w-25">
                                         <svg class="w-100 p-4" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 193.05 184.57"><defs><style>.cls-1{fill:#cb6ce6;}.cls-2{fill:#fff;}</style></defs><title>Ativo 1</title><g id="Camada_2" data-name="Camada 2"><g id="Camada_1-2" data-name="Camada 1"><path class="cls-1" d="M135.53,86.89q-15.3,28-30.6,55.92c-1.29,2.39-3.6,4.56-6.46,4.64s-5.06-1.85-6.28-4.13Q75,112.09,57.82,80.87c-1.52-2.51-1.72-6.1.49-8.29a7.56,7.56,0,0,1,4.57-2.13Q74.69,91.9,86.5,113.36a5.54,5.54,0,0,0,4.85,3.2c2.21-.06,4-1.73,5-3.58l21.39-39.22,11.16.72a8.66,8.66,0,0,1,6.91,3.72C137.5,80.82,137,84.29,135.53,86.89ZM107.81,74.35a3.74,3.74,0,0,0-3-1.62q-13.85-.9-27.7-1.8a3.71,3.71,0,0,0-3,1,3,3,0,0,0-.21,3.61l15,27.2a3.12,3.12,0,0,0,2.73,1.8,3.54,3.54,0,0,0,2.82-2l13.3-24.35A3.69,3.69,0,0,0,107.81,74.35Z"></path><path class="cls-1" d="M5.48,30.54H25.34a3.68,3.68,0,0,0,3.05-1.25L47.29,0H27.59l-25,27.66C1.43,29,3,30.54,5.48,30.54Z"></path><path class="cls-1" d="M60.69,30.54h18c2.29,0,4.19-1.05,4.37-2.42L86.7,0H67L56.41,27.33C55.78,29,57.87,30.54,60.69,30.54Z"></path><path class="cls-1" d="M115,30.54h16.6c3.23,0,5.63-1.8,4.9-3.69L126.1,0H106.4L110,27.76C110.18,29.33,112.37,30.54,115,30.54Z"></path><path class="cls-1" d="M169.8,30.54h16.25c3.18,0,5.21-2,3.69-3.71L165.51,0h-19.7L164,28.15C164.9,29.59,167.21,30.54,169.8,30.54Z"></path><path class="cls-1" d="M13.81,55.39A13.81,13.81,0,0,0,27.63,41.58v-2.3a3,3,0,0,0-3-3H3a3,3,0,0,0-3,3v2.3A13.81,13.81,0,0,0,13.81,55.39Z"></path><path class="cls-1" d="M69,55.39A13.81,13.81,0,0,0,82.8,41.58V40.07a3.75,3.75,0,0,0-3.75-3.75H58.92a3.75,3.75,0,0,0-3.75,3.75v1.51A13.81,13.81,0,0,0,69,55.39Z"></path><path class="cls-1" d="M114.05,36.32a3.75,3.75,0,0,0-3.75,3.75v1.51a13.82,13.82,0,0,0,27.63,0V40.07a3.75,3.75,0,0,0-3.76-3.75Z"></path><path class="cls-1" d="M189.3,36.32H169.18a3.75,3.75,0,0,0-3.76,3.75v1.51a13.81,13.81,0,0,0,13.82,13.81h0a13.81,13.81,0,0,0,13.81-13.81V40.07A3.75,3.75,0,0,0,189.3,36.32Z"></path><polygon class="cls-2" points="14.74 63.87 14.74 166.04 95.91 166.04 97.19 166.04 178.35 166.04 178.35 63.87 168.52 63.87 168.52 156.31 97.19 156.31 95.91 156.31 24.57 156.31 24.57 63.87 14.74 63.87"></polygon><path class="cls-2" d="M96.55,170.78H2.33S1,184.57,17,184.57H176.11c15.95,0,14.65-13.79,14.65-13.79Z"></path></g></g></svg>
                                             <div class="card-body">
-                                                <span style="color: var(--roxo-vendex); font-weight: 700;" class="CardTexto">
+                                                <span class="CardTexto">
                                                     <h5 class="card-title">Consultoria</h5>
                                                     <h5 class="card-title">De Venda</h5>
                                                 </span>
@@ -171,10 +171,10 @@
                                     </div>
                                   <!--Consultoria-->
                                   <!--Ads-->
-                                    <div id="Cards03" class="card bg-black text-center d-flex justify-content-center" style="width: 18rem;">
+                                    <div id="Cards03" class="card bg-black text-center d-flex justify-content-center  w-25">
                                         <svg class="w-100 p-4" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" style="fill: rgba(203,108,230);transform: ;msFilter:;"><path class="lupa" d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path></svg>
                                         <div class="card-body">
-                                            <span style="color: var(--roxo-vendex); font-weight: 700;" class="CardTexto">
+                                            <span class="CardTexto">
                                                 <h5 class="card-title">Gestão de</h5>
                                                 <h5 class="card-title">ADS</h5>
                                             </span>
@@ -185,8 +185,8 @@
                                     </div>
                                     <!--Ads-->
                                     <!--Automações-->
-                                        <div id="Cards04" class="card bg-black text-center d-flex justify-content-center" style="width: 18rem;">
-                                            <svg class="w-100 p-3" id="Grupo_54" data-name="Grupo 54" xmlns="http://www.w3.org/2000/svg" width="159.532" height="206.158" viewBox="0 0 159.532 206.158">
+                                        <div id="Cards04" class="card bg-black text-center d-flex justify-content-center  w-25">
+                                            <svg class="w-100 p-4" id="Grupo_54" data-name="Grupo 54" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 159.532 206.158">
                                                 <path id="Caminho_86" data-name="Caminho 86" d="M-2378.313-460.439q-16.9,30.894-33.809,61.8c-1.433,2.646-3.987,5.041-7.147,5.126-3.007.156-5.589-2.037-6.935-4.559q-18.984-34.517-37.983-69.014c-1.677-2.773-1.9-6.742.54-9.16a8.378,8.378,0,0,1,5.058-2.36q13.044,23.718,26.1,47.427c1.044,1.95,3.036,3.645,5.357,3.528,2.437-.063,4.405-1.91,5.509-3.955q11.829-21.663,23.647-43.337l12.328.795a9.544,9.544,0,0,1,7.637,4.114C-2376.133-467.146-2376.657-463.313-2378.313-460.439Zm-30.63-13.859a4.161,4.161,0,0,0-3.325-1.793q-15.3-.993-30.609-1.987a4.139,4.139,0,0,0-3.34,1.06,3.3,3.3,0,0,0-.233,3.987q8.264,15.036,16.545,30.06a3.435,3.435,0,0,0,3.02,1.989,3.89,3.89,0,0,0,3.109-2.233q7.355-13.446,14.7-26.9A4.08,4.08,0,0,0-2408.943-474.3Z" transform="translate(2536.351 521.828)" fill="#cb6ce6"></path>
                                                 <path id="Caminho_87" data-name="Caminho 87" d="M-2373.163-352.864l2.405,13.584-68.865,36.715-32.05-181.089,77.284,10.85,2.4,13.584,3.356-.6-2.509-14.164c-.652-3.682-3.211-7.028-5.77-7.483l-74.947-13.359a7.1,7.1,0,0,0-2.191-.034l-.008-.042-13.126,2.318c-3.385.6-5.591,4.138-4.924,7.9l33.642,190.088c.667,3.767,3.952,6.334,7.338,5.735l12.23-2.164,0-.045a6.885,6.885,0,0,0,2.935-.948l65.811-38.272c2.247-1.306,3.5-5.327,2.851-9.009l-2.506-14.164Z" transform="translate(2492.224 494.926)" fill="#cb6ce6"></path>
                                                 <path id="Caminho_88" data-name="Caminho 88" d="M-2448.843-446.562h-13.645a3.913,3.913,0,0,1-3.9-3.9h0a3.911,3.911,0,0,1,3.9-3.9h13.645a3.912,3.912,0,0,1,3.9,3.9h0A3.913,3.913,0,0,1-2448.843-446.562Z" transform="translate(2534.827 561.816)" fill="#fff"></path>
@@ -194,9 +194,12 @@
                                                 <path id="Caminho_90" data-name="Caminho 90" d="M-2446.7-451.767h-21.83a4.1,4.1,0,0,1-4.093-4.093h0a4.1,4.1,0,0,1,4.093-4.093h21.83a4.106,4.106,0,0,1,4.1,4.093h0A4.106,4.106,0,0,1-2446.7-451.767Z" transform="translate(2524.542 552.597)" fill="#fff"></path>
                                               </svg>
                                             <div class="card-body">
-                                            <h5 style="color: var(--roxo-vendex); font-weight: 700;" class="card-title">Automações</h5>
-                                            <br>
-                                            <a href="#" id="LinkCards" style="background: var(--background-vendx); border: none;" class="btn btn-primary w-75">Saiba Mais</a>
+                                                <span class="CardTexto">
+                                                    <h5 class="card-title">Automações</h5>
+                                                </span>
+                                                <span>
+                                                    <a href="#" id="LinkCards" style="background: var(--background-vendx); border: none;" class="btn btn-primary w-75">Saiba Mais</a>
+                                                </span>
                                             </div>
                                         </div>
                                   <!--Automações-->
