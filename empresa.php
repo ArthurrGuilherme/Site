a5bacd1c77245032254dd54cf0b7d9f1c2ece381
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-09-30 19:11:10 -0300

    Sobre nos

--- empresa.php
+++ empresa.php
@@ -45,5 +45,73 @@
             <!--Apresentação-->
         </section>
     <!--Container principal das paginas estaticas-->  
+    <!--Conteudo principal-->
+        <main>
+            <!--Serviços-->
+                <section class="p-5">
+                    <article>
+                        <!--Texto-->
+                        <div>
+                            <span class="d-flex flex-column align-items-center text-center">
+                                <!--Titulo-->
+                                    <h4 class="text-light">Os melhores profissionais a seu serviço!</h4>
+                                <!--Titulo-->
+                                <!--Texto-->
+                                    <span class="text-light">
+                                        <p >Profissionais certificados para turbinar suas vendas com estratégias personalizadas com foco total em</p>
+                                        <p>performance. Aqui trabalhamos em 04 verticais: Estrategista de Marketing & SEO, estrategista de ADS,</p>
+                                        <p>estrategista de precificação para maximizar a sua margem de lucro e gestor de e-commerce, para</p>
+                                        <p>garantir seu sucesso nos maiores marketplaces do pais.</p>
+                                    </span>
+                                <!--Texto-->
+                            </span>
+                        </div>
+                    </article>
+                </section>
+            <!--Serviços-->
+            <!--Soluções-->
+                <section>
+                    <article class="container-md d-flex justify-content-evenly align-items-center">
+                        <!--Texto-->
+                            <span id="SessaoTexto" style="color: var(--roxo-vendex);">
+                                <!--Titulo-->
+                                    <h4 class="text-light">Sobre nós</h4>
+                                    <h3>CONTAMOS COM UM TIME DE</h3> 
+                                    <h3>ESPECIALISTAS PARA</h3>
+                                    <h3>GERENCIAR O SEU NEGÓCIO!</h3>
+                                <!--Titulo-->
+                                <br>
+                                <!--Legenda-->
+                                    <span class="text-light">
+                                        <p>Somos uma empresa tecnológica, com profissionais</p>
+                                        <p>cuidadosamente capacitados e certificados para oferecer a</p>
+                                        <p>melhor assessoria e gestão nos maiores marketplaces do país.</p>
+                                        <p>Nossas soluções vão desde a implantação de sistemas,</p>
+                                        <p>passando por cadastro e otimização de anúncios, até a</p>
+                                        <p>aplicação de estratégias avançadas de marketing para</p>
+                                        <p>aumentar o faturamento de seu negócio.</p>
+                                    </span>
+                                <!--Legenda-->
+                                <br>
+                                <!--Legenda-->
+                                    <span class="text-light">
+                                        <p>Dispomos de uma equipe de profissionais experientes em e-</p>
+                                        <p>commerce, aplicando métodos e estratégias de posicionamento</p>
+                                        <p>de mercado, estratégia de operações e de crescimento para</p>
+                                        <p>ganhar mercado, garantindo o seu sucesso.</p>
+                                    </span>
+                                <!--Legenda-->
+                            </span>    
+                        <!--Texto-->
+                        <!--Img-->
+                            <span>
+                                <img class="w-50" id="Img" src="./img/empresaSobrenos.webp" alt="Imagem 01">
+                            </span>
+                        <!--Img-->
+                    </article>
+                </section>
+            <!--Soluções-->
+        </main>
+    <!--Conteudo principal-->
 </body>
 </html>
