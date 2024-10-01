1c7e2e3cfd077d3806a6cc0359c41b356b91d28f
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-10-01 08:25:39 -0300

    Serviços

--- gdm.php
+++ gdm.php
@@ -108,11 +108,67 @@
                         </span>    
                     <!--Texto-->
                     <!--Img-->
-                        <img style="width: 30% !important;" class="w-25" id="Img" src="./img/gestao-marketplace-servicos.webp" alt="Imagem 01">
+                        <img style="width: 30% !important; border-radius: 16px" class="w-25" id="Img" src="./img/gestao-marketplace-servicos.webp" alt="Imagem 01">
                     <!--Img-->
                 </article>
             </section>
         <!--Sobre Nós-->
+        <!--Serviços-->
+            <section class="p-5">
+                <article class="container-md d-flex justify-content-evenly align-items-center">
+                    <!--Img-->
+                        <img style="width: 40% !important; border-radius: 0px" class="w-25" id="Img" src="./img/gestao-marketing-servicos.webp" alt="Imagem 01">
+                    <!--Img-->
+                    <!--Texto-->
+                        <span class="d-flex flex-column" id="SessaoTexto" style="color: var(--roxo-vendex); gap: 0.6rem">
+                            <!--Titulo-->
+                                <h4 class="text-light">Gestão completa do seu funil de vendas</h4>
+                                <h3>CRIAÇÃO DE ESTRATÉGIA</h3>
+                            <!--Titulo-->
+                            <!--Legenda-->
+                                <span style="font-size: 0.8rem;" class="text-light">
+                                    <p><strong style="color: var(--rosa-vendex);">Ranqueamento dos Anúncios:</strong> Quem não é visto, não é</p>
+                                    <p>lembrado, concorda?</p>
+                                    <p>Aqui na Vendex, nossos especialistas dominam as técnicas e</p>
+                                    <p>algoritmos dos marketplaces para posicionar sempre seus</p>
+                                    <p>anúncios nas primeiras páginas e assim vender muito. Nosso</p>
+                                    <p>objetivo aqui é garantir que seus produtos sejam vistos por</p>
+                                    <p>seus clientes no momento da busca, otimizando assim a</p>
+                                    <p>conversão de vendas.</p>
+                                </span>
+                                <span style="font-size: 0.8rem;" class="text-light">
+                                    <p><strong style="color: var(--rosa-vendex);">Gestão e Estratégias de Campanhas de ADS:</strong>A gestão eficiente</p>
+                                    <p>das estratégias de campanhas é fundamental para o sucesso</p>
+                                    <p>nos marketplaces. Nosso time irá criar e administrar suas</p>
+                                    <p>campanhas buscando garantir o máximo retorno dos seus</p>
+                                    <p>investimentos</p>
+                                </span>
+                            <!--Legenda-->
+                            <!--Legenda-->
+                                <span style="font-size: 0.8rem;" class="text-light">
+                                    <p><strong style="color: var(--rosa-vendex);">Estratégias de ACOS:</strong>Nossa prioridade é a saúde financeira do</p>
+                                    <p>das estratégias de campanhas é fundamental para o sucesso</p>
+                                    <p>nos marketplaces. Nosso time irá criar e administrar suas</p>
+                                    <p>campanhas buscando garantir o máximo retorno dos seus</p>
+                                    <p>investimentos</p>
+                                </span>
+                            <!--Legenda-->
+                            <!--Legenda-->
+                                <span style="font-size: 0.8rem;" class="text-light">
+                                    <p><strong style="color: var(--rosa-vendex);">Estratégias SEO:</strong>A análise de SEO é um processo essencial no</p>
+                                    <p>marketing digital que envolve a avaliação detalhada de um</p>
+                                    <p>site ou conteúdo online para otimizar sua visibilidade nos</p>
+                                    <p>mecanismos de busca, como o Google.</p>
+                                    <p>Com essa estratégia vamos aumentar o tráfego orgânico dos</p>
+                                    <p>seus anúncios, melhorar o posicionamento nos resultados de</p>
+                                    <p>pesquisa e, consequentemente, aumentar o funil de vendas.</p>
+                                </span>
+                            <!--Legenda-->
+                        </span>    
+                    <!--Texto-->
+                </article>
+            </section>
+        <!--Serviços-->
     </main>
 </body>
     <?php echo $headjs ?>
