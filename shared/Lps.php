8c9459371282c4f6701ee7c9a4f853eba0b08f0c
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-10-03 08:10:13 -0300

    ads01

--- shared/Lps.php
+++ shared/Lps.php
@@ -83,6 +83,30 @@
                                         </div>
     '
     ?>
+    <?php $ads01 = '
+        <div id="Cards03" class="card bg-black text-center d-flex justify-content-center  w-25">
+                                            <svg class="w-100 p-4" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" style="fill: rgba(203,108,230);transform: ;msFilter:;"><path class="lupa" d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path></svg>
+                                            <div class="card-body">
+                                                <span class="CardTexto">
+                                                    <h5 class="card-title">Gestão de</h5>
+                                                    <h5 class="card-title">ADS</h5>
+                                                </span>
+                                                
+                                                    <span class="CardTexto text-light">
+                                                        <p>A automação com o</p>
+                                                        <p>sistema ERP alavanca</p>
+                                                        <p>as vendas de seu</p>
+                                                        <p>negócio organizando de</p>
+                                                        <p>forma automatizada e</p>
+                                                        <p>simplificada</p>
+                                                    </span>
+                                                <span>
+                                                    <a href="#" id="LinkCards" style="background: var(--background-vendx); border: none;" class="btn btn-primary w-75">Saiba Mais</a>
+                                                </span>    
+                                            </div>
+                                        </div>
+    '
+    ?>
     <?php $automacoes = '
         <div id="Cards04" class="card bg-black text-center d-flex justify-content-center  w-25">
                                                 <svg class="w-100 p-4" id="Grupo_54" data-name="Grupo 54" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 159.532 206.158">
