4102a15d45186a99a16daac9d4d50f34b5c14e29
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-10-04 09:50:21 -0300

    CDV 04/10/2024

---
+++ CDV.php
@@ -0,0 +1,56 @@
+<?php 
+    include './shared/shared.php';
+    include './shared/head.php';
+    include './shared/Lps.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pagina sobre a empresa">
+    <title>Consultoria de Vendas - Vendex</title>
+    <!--Css/js principais-->
+        <?php echo $headcss ?>
+    <!--Css/js principais-->
+    <!--Api-->
+        <!--boxicons-->
+            <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <!--boxicons-->
+    <!--Api-->   
+</head>
+<body>
+    <!--Container principal das paginas estaticas-->
+        <section  style="background: url(./img/bn-empresa.webp); background-size: cover">
+            <!--Header Primario-->
+                <?php echo $headerprincial ?>
+            <!--Header Primario-->
+            <!--Header Secundario-->
+                <?php echo $headersecundario ?>
+            <!--Header Primario-->
+            <!--Apresentação-->
+                <section style="padding: 5rem 0px 10rem 0px;" class="container-md d-flex justify-content-between" id="ContainerApresentacao">
+                    <!--Estrutura texto-->
+                        <article class="d-flex flex-column">
+                            <!--Titulo-->
+                                <h1 style="padding-bottom: 2rem; font-weight: 700;">Gestão de Marketplace</h1>
+                            <!--Titulo-->
+                            <!--Texto-->
+                                <strong ><p style="font-weight: 900; font-size: 1.1rem">Descubra o que nossos especialistas podem</p>
+                                <p style="font-weight: 900; font-size: 1.1rem">fazer por sua empresa!</p></strong>
+                            <!--Texto-->
+                        </article>
+                    <!--Estrutura texto-->
+                </section>
+            <!--Apresentação-->
+        </section>
+    <!--Container principal das paginas estaticas-->  
+    <main>
+
+    </main>
+    <!--Footer-->
+        <?php echo $footer ?>  
+    <!--Footer-->
+</body>
+    <?php echo $headjs ?>
+</html>
