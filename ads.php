ae0b7b91a160b3afb7fd8b41209196818a363866
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-10-08 08:15:04 -0300

    08/10

---
+++ ads.php
@@ -0,0 +1,28 @@
+<?php 
+    include './shared/shared.php';
+    include './shared/head.php';
+    include './shared/Lps.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pagina sobre a empresa">
+    <title>Gestão de Marketplace - Vendex</title>
+    <!--Css/js principais-->
+        <?php echo $headcss ?>
+    <!--Css/js principais-->
+    <!--Api-->
+        <!--boxicons-->
+            <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <!--boxicons-->
+    <!--Api-->   
+</head>
+<body>
+    <main>
+
+    </main>
+</body>
+    <?php echo $headjs ?>
+</html>
