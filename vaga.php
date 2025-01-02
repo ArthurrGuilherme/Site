96ae3bf32e559f998889440ee3016c736c982bdb
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-01-01 23:22:23 -0300

    vagas

---
+++ vaga.php
@@ -0,0 +1,67 @@
+<?php 
+    include './shared/shared.php';
+    include './shared/head.php';
+    include './shared/Lps.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pagina sobre a empresa">
+    <title>Trabalhe Conosco - Vendex</title>
+    <!--Css/js principais-->
+        <?php echo $headcss ?>
+    <!--Css/js principais-->
+    <!--Api-->
+        <!--boxicons-->
+            <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <!--boxicons-->
+    <!--Api-->   
+</head>
+<body>
+    <!--Container principal das paginas estaticas-->
+        <section  style="background: url(./img/bn-empresa.webp); background-size: cover">
+            <!--Header Primario-->
+                <?php echo $headerprincial ?>
+            <!--Header Primario-->
+            <!--Header Secundario-->
+                <?php echo $headersecundario ?>
+            <!--Header Primario-->
+            <!--Apresentação-->
+                <section style="padding: 5rem 0px 10rem 0px;" class="container-md d-flex justify-content-between" id="ContainerApresentacao">
+                    <!--Estrutura texto-->
+                        <article class="d-flex flex-column">
+                            <!--Titulo-->
+                                <h1 style="padding-bottom: 2rem; font-weight: 700;">Nossa Empresa</h1>
+                            <!--Titulo-->
+                            <!--Texto-->
+                                <strong ><p style="font-weight: 900; font-size: 1.1rem">Nossa especialidade é aumentar as vendas do seu e-commerce</p>
+                                <p style="font-weight: 900; font-size: 1.1rem">através das maiores plataformas dos marketplaces do país</p></strong>
+                            <!--Texto-->
+                        </article>
+                    <!--Estrutura texto-->
+                </section>
+            <!--Apresentação-->
+        </section>
+    <!--Container principal das paginas estaticas-->  
+    <!--Conteudo principal-->
+        <main>
+
+        </main>
+    <!--Conteudo principal-->
+    <!--Footer-->
+        <?php echo $footer ?>  
+    <!--Footer-->
+</body>
+    <?php echo $headjs ?>
+    <script>
+    document.querySelector("#Btnconato03").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    scrollto("#formulario")
+
+    console.log("go to up")
+    })                 
+    </script>
+</html>
