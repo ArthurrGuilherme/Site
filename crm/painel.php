ef9bb6dcdee1797c3d40f79153fcf06b3513dcb4
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-01-08 16:33:14 -0300

    Add files via upload

---
+++ crm/painel.php
@@ -0,0 +1,24 @@
+<?php 
+    include './shared/shared.php';
+    include './shared/head.php';
+    include './shared/Lps.php';
+?>
+<!DOCTYPE html>
+<html lang="Pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vendex - Painel</title>
+    <!--Css/js principais-->
+        <?php echo $headcss ?>
+    <!--Css/js principais-->
+    <!--Api-->
+        <!--boxicons-->
+            <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <!--boxicons-->
+    <!--Api-->  
+</head>
+<body>
+    
+</body>
+</html>
