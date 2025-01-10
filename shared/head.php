0844908b013e333d6617c7854eb305bbc2df5f5b
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-01-10 09:19:02 -0300

    Add files via upload

--- shared/head.php
+++ shared/head.php
@@ -42,10 +42,10 @@
   <head>
         <div class="container-fluid">
         <div class="row flex-nowrap">
-            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+            <div style="background: #b56afc" class="col-auto col-md-3 col-xl-2 px-sm-2 px-0">
                 <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                     <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                        <span class="fs-5 d-none d-sm-inline">Vendex Company</span>
+                        <span style="Color: #000" class="fs-5 d-none d-sm-inline">Vendex Company</span>
                     </a>
                     <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                         <li class="nav-item">
